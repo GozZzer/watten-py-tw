45f7f515bf10c5f45cd43cf48b365b8dde3715df
GozZzer <74556139+GozZzer@users.noreply.github.com>
2022-07-03 23:05:22 +0200

    Commenting Everything 0.1

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+# Import The Client from the watten_py module
 from watten_py import WattenClient
 
 client = WattenClient()
+# Run the Client and stop it when KeyboardInterrupt occurs
 try:
     client.run()
 except KeyboardInterrupt:
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
+# Import The Server from the watten_py module
 from watten_py import WattenServer
 
 server = WattenServer()
+# Run the Server and stop it when KeyboardInterrupt occurs
 try:
     server.run()
 except KeyboardInterrupt:
